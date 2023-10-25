4ee05434081c46793295dbfe4fcef437753d551c
Dean Sinnott <20067691@mail.wit.ie>
2023-10-25 16:42:45 +0100

    Adding filtering of movie list.

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -40,8 +40,8 @@ export default function FilterMoviesCard(props) {
 
   const handleChange = (e, type, value) => {
     e.preventDefault()
-    // Completed later
-  };
+    props.onUserInput(type, value)   // NEW
+  }
   const handleTextChange = e => {
     handleChange(e, "name", e.target.value)
   }
@@ -75,6 +75,9 @@ export default function FilterMoviesCard(props) {
           <Select
             labelId="genre-label"
             id="genre-select"
+            defaultValue=""
+            value={props.genreFilter}
+            onChange={handleGenreChange}
           >
             {genres.map((genre) => {
               return (
