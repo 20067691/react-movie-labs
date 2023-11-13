c65cfe3996a2b51b23a4dacbeea5b8404d7b37ff
DeanS <20067691@mail.wit.ie>
2023-11-13 19:36:29 +0000

    Added top movie card, styled and set only 1 poster to movie details

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -4,19 +4,26 @@ import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import { getTopRatedMovie } from "../api/tmdb-api";
+import TopRatedMovieCard from "../components/topRatedMovieCard";
 
 const HomePage = (props) => {
 
   const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
+  const {  data: topMovie, error: topMovieError, isLoading: topMovieLoading, isError: topMovieIsError }  = useQuery('topRated', getTopRatedMovie)
 
-  if (isLoading) {
+  if (isLoading || topMovieLoading) {
     return <Spinner />
   }
 
-  if (isError) {
-    return <h1>{error.message}</h1>
+  if (isError || topMovieIsError) {
+    return <h1>{error.message || topMovieError.message}</h1>
   }  
   const movies = data.results;
+  const topRatedMovie = topMovie.results[0];
+  console.log(topRatedMovie);
+
+  
 
   // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
@@ -24,6 +31,12 @@ const HomePage = (props) => {
   const addToFavorites = (movieId) => true 
 
   return (
+    <>
+    <TopRatedMovieCard topRatedMovie={topRatedMovie}
+      
+    />
+    
+
     <PageTemplate
       title="Discover Movies"
       movies={movies}
@@ -31,6 +44,7 @@ const HomePage = (props) => {
         return <AddToFavoritesIcon movie={movie} />
       }}
     />
+    </>
 );
 };
 export default HomePage;
